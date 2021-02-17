4bf34a3d4a664c8d4787691ad68b17e55ad230d1
Xiao <mail52@26.com>
2021-02-17 10:59:36 +0800

    test

--- app/Http/Controllers/Api/MyShareController.php
+++ app/Http/Controllers/Api/MyShareController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MyShareResource;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\Post;
 class MyShareController extends Controller
 {
     public function index()
